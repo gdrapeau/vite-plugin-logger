ca5f2f9615b0339edae5ea5c5bc8529480cea88c
gdrapeau <github@r1d3.com>
2025-06-19 09:19:36 -0400

    add logSource to  index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-export function logger(message) {
+export function logger(message,logSource) {
     return {
         name: 'logger',
         transform: function(src, id) {
             console.log(message,id)
+            if (logSource) console.log('source=\n',src)
             return undefined
         }
     }
